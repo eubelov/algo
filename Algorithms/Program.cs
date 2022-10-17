bfed8476ee5ede97b183b707782315f151d5fe7e
Evgeny Belov <evgeny.belov@vista.co>
2022-10-17 13:06:14 +1300

    PacificAtlantic flow solution

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.Write("Hello =)");
+﻿using Algorithms.LeetCode.ArraysAndHashing;
+using Algorithms.LeetCode.Graphs;
+
+Console.Write("Hello =)");
+
+//new TowSumsSolution().TwoSum(new[]{3,2,3}, 6);
+//var res = new GroupAnagramsSolution().GroupAnagrams(new[] { "" });
+//new MostFrequentElements().TopKFrequent(new[]{1}, 1);
+
+new WaterFlow().PacificAtlantic2(
+    new[]
+    {
+        new[] { 1, 2 },
+        new[] { 4, 3 },
+    });
