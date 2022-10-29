891706d5f5e92eeabd9af1c6571fe4239c3c7167
Evgeny Belov <domolink1@gmail.com>
2022-10-30 09:40:17 +1300

    Generate Parenthesis

---
+++ Algorithms/LeetCode/Stack/GenerateParenthesisSolution.cs
@@ -0,0 +1,35 @@
+namespace Algorithms.LeetCode.Stack;
+
+/// <summary>
+/// https://leetcode.com/problems/generate-parentheses/
+/// </summary>
+public class GenerateParenthesisSolution
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var results = new List<string>();
+
+        void Backtrack(int open, int closed, string c)
+        {
+            if (open == closed && open == n)
+            {
+                results.Add(c);
+                return;
+            }
+
+            if (open < n)
+            {
+                Backtrack(open + 1, closed, c + '(');
+            }
+
+            if (closed < open)
+            {
+                Backtrack(open, closed + 1, c + ')');
+            }
+        }
+
+        Backtrack(0, 0, string.Empty);
+
+        return results;
+    }
+}
